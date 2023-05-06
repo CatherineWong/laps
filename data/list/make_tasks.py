2c2a49218b4525c4845218da8628e66a8e9e0d19
Sam Acquaviva <samacqua@gmail.com>
2023-05-06 14:58:31 -0400

    added blank files for synthetic language.

--- data/list/make_tasks.py
+++ data/list/make_tasks.py
@@ -20,8 +20,8 @@ DEFAULT_DATA_DIRECTORY = os.path.join(ROOT_DIR, f"dreamcoder/data/")
 DEFAULT_TRAIN_TEST_SPLIT = 0.8
 
 DEFAULT_DATASET = "Lucas-depth3"
-DEFAULT_TASKS_DIRECTORY = os.path.join(DEFAULT_DATA_DIRECTORY, TASKS)
-DEFAULT_LANGUAGE_DIRECTORY = os.path.join(DEFAULT_DATA_DIRECTORY, LANGUAGE)
+DEFAULT_TASKS_DIRECTORY = os.path.join(ROOT_DIR, f"dreamcoder/data/")
+DEFAULT_LANGUAGE_DIRECTORY = os.path.join(ROOT_DIR, f"data/{DOMAIN_NAME}/language/")
 
 
 @TaskLoaderRegistry.register
@@ -31,8 +31,8 @@ class Re2Loader(TaskDataLoader):
     def load_tasks(self):
         """Loads the list domain tasks. Largely taken from the list domain's main.py file."""
 
-        old_list_tasks_path = os.path.join(DEFAULT_DATA_DIRECTORY, "list_tasks.json")
-        new_list_tasks_path = os.path.join(DEFAULT_DATA_DIRECTORY, "list_tasks2.json")
+        old_list_tasks_path = os.path.join(DEFAULT_TASKS_DIRECTORY, "list_tasks.json")
+        new_list_tasks_path = os.path.join(DEFAULT_TASKS_DIRECTORY, "list_tasks2.json")
 
         tasks = {
             "Lucas-old": lambda: retrieveJSONTasks(old_list_tasks_path) + sortBootstrap(),
@@ -46,5 +46,19 @@ class Re2Loader(TaskDataLoader):
         test_tasks, train_tasks = testTrainSplit(tasks, DEFAULT_TRAIN_TEST_SPLIT)
 
         tasks = {TRAIN: train_tasks, TEST: test_tasks}
+        # for split in tasks.keys():
+        #     for t in tasks[split]:
+        #         t.supervisedSolution = Program.parse(t.name).betaNormalForm()
+        #         t.groundTruthProgram = t.supervisedSolution
 
         return tasks
+
+@TaskLanguageLoaderRegistry.register
+class Re2SyntheticLanguageLoader(TaskDataLoader):
+    name = "list_synthetic"
+
+    def load_task_language(self):
+        dataset_path = os.path.join(
+            DEFAULT_LANGUAGE_DIRECTORY, DEFAULT_DATASET, SYNTHETIC
+        )
+        return self.load_task_language_for_directory(dataset_path, splits=[TRAIN, TEST])
--- data/list/test_make_tasks.py
+++ data/list/test_make_tasks.py
@@ -7,4 +7,7 @@ import data.list.make_tasks as to_test
 
 
 def test_load_list_tasks():
-    pass
+    task_loader = TaskLoaderRegistry[to_test.Re2Loader.name]
+    tasks = task_loader.load_tasks()
+    for split in tasks:
+        print(split, len(tasks[split]))
